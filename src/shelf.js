ade8bef11d89e1a1a80cbffbf30f814c8f32a517
meikhaill <meikhailmajor9@gmail.com>
2024-03-18 11:24:01 -0600

    update

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,23 @@
 
+
+function shelfBook(book, shelf) {
+  if(shelf.length <= 2) {
+    return shelf.push(book);
+  } else {
+    return false
+  }
+}
+
+function unshelfBook(book, shelf) {
+  indx = shelf.indexOf(book)
+  if(shelf.includes(book)) {
+    
+  }
+}
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
